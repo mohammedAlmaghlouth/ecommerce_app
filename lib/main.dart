1ca761e747914ff3fe06ba9d0669f7d7bf432d1c
Mohammed Al Maghlouth <65549274+mohammedAlmaghlouth@users.noreply.github.com>
2023-04-06 04:47:44 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:ecommerce_app/src/app.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:flutter/material.dart';
@@ -7,15 +6,21 @@ import 'package:flutter/material.dart';
 void main() async {
   // * For more info on error handling, see:
   // * https://docs.flutter.dev/testing/errors
+  /// error handler that caught all types
   await runZonedGuarded(() async {
+    // ensure the binding is initialized, espically to prevent package to mess with it
     WidgetsFlutterBinding.ensureInitialized();
     // * Entry point of the app
     runApp(const MyApp());
 
     // * This code will present some error UI if any uncaught exception happens
+
+    // this simple handler to caught error in print them in screen
     FlutterError.onError = (FlutterErrorDetails details) {
       FlutterError.presentError(details);
     };
+
+    // this is backup one, with design
     ErrorWidget.builder = (FlutterErrorDetails details) {
       return Scaffold(
         appBar: AppBar(
@@ -25,7 +30,9 @@ void main() async {
         body: Center(child: Text(details.toString())),
       );
     };
-  }, (Object error, StackTrace stack) {
+  }, 
+  // this is second argument of runZonedGuarded, if neither ui handlers apply , it will apply this 
+  (Object error, StackTrace stack) {
     // * Log any errors to console
     debugPrint(error.toString());
   });
