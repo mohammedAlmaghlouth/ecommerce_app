8a62b5fb1b74a3d05270e68963cf98562e9753a7
Mohammed Al Maghlouth <65549274+mohammedAlmaghlouth@users.noreply.github.com>
2023-04-08 07:18:40 +0300

    gr_ch+rv

--- lib/src/features/product_page/leave_review_action.dart
+++ lib/src/features/product_page/leave_review_action.dart
@@ -1,11 +1,12 @@
-import 'package:ecommerce_app/src/features/leave_review_page/leave_review_screen.dart';
 import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
 import 'package:ecommerce_app/src/models/purchase.dart';
+import 'package:ecommerce_app/src/router/app_router.dart';
 import 'package:ecommerce_app/src/utils/date_formatter.dart';
 import 'package:flutter/material.dart';
 import 'package:ecommerce_app/src/common_widgets/custom_text_button.dart';
 import 'package:ecommerce_app/src/common_widgets/responsive_two_column_layout.dart';
 import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:go_router/go_router.dart';
 
 /// Simple widget to show the product purchase date along with a button to
 /// leave a review.
@@ -37,13 +38,11 @@ class LeaveReviewAction extends StatelessWidget {
               text: 'Leave a review'.hardcoded,
               style: Theme.of(context)
                   .textTheme
-                  .bodyText1!
+                  .bodyLarge!
                   .copyWith(color: Colors.green[700]),
-              onPressed: () => Navigator.of(context).push(
-                MaterialPageRoute(
-                  fullscreenDialog: true,
-                  builder: (_) => LeaveReviewScreen(productId: productId),
-                ),
+              onPressed: () => context.pushNamed(
+                AppRoute.leaveReview.name,
+                params: {'id': productId},
               ),
             ),
           ),
--- lib/src/router/app_router.dart
+++ lib/src/router/app_router.dart
@@ -1,4 +1,6 @@
 import 'package:ecommerce_app/src/features/account/account_screen.dart';
+import 'package:ecommerce_app/src/features/checkout/checkout_screen.dart';
+import 'package:ecommerce_app/src/features/leave_review_page/leave_review_screen.dart';
 import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
 import 'package:ecommerce_app/src/features/orders_list/orders_list_screen.dart';
 import 'package:ecommerce_app/src/features/shopping_cart/shopping_cart_screen.dart';
@@ -13,9 +15,11 @@ import '../features/products_list/products_list_screen.dart';
 enum AppRoute {
   home,
   cart,
+  leaveReview,
   orders,
   account,
   signIn,
+  checkout,
   product,
 }
 
@@ -29,16 +33,30 @@ final goRouter = GoRouter(
       builder: ((context, state) => const ProductsListScreen()),
       routes: [
         GoRoute(
-            path: 'product/:id',
-            name: AppRoute.product.name,
-            builder: (context, state) {
-              // state.params['id'] is to create a key and paramas argument,
-              // and the key will retrieved when the user enter the value inside call parameter
-              // then it will be stored inside productId
-              final productId = state.params['id']!;
-              // pass the productId inside the return screen widget & Inside :id in url
-              return ProductScreen(productId: productId);
-            }),
+          path: 'product/:id',
+          name: AppRoute.product.name,
+          builder: (context, state) {
+            // state.params['id'] is to create a key and paramas argument,
+            // and the key will retrieved when the user enter the value inside call parameter
+            // then it will be stored inside productId
+            final productId = state.params['id']!;
+            // pass the productId inside the return screen widget & Inside :id in url
+            return ProductScreen(productId: productId);
+          },
+          routes: [
+            GoRoute(
+                path: 'review',
+                name: AppRoute.leaveReview.name,
+                pageBuilder: (context, state) {
+                  final productId = state.params['id']!;
+                  return MaterialPage(
+                    key: state.pageKey,
+                    fullscreenDialog: true,
+                    child: LeaveReviewScreen(productId: productId),
+                  );
+                }),
+          ],
+        ),
         GoRoute(
           path: 'cart',
           name: AppRoute.cart.name,
@@ -47,6 +65,17 @@ final goRouter = GoRouter(
             fullscreenDialog: true,
             child: const ShoppingCartScreen(),
           ),
+          routes: [
+            GoRoute(
+              path: 'checkout',
+              name: AppRoute.checkout.name,
+              pageBuilder: (context, state) => MaterialPage(
+                key: state.pageKey,
+                fullscreenDialog: true,
+                child: const CheckoutScreen(),
+              ),
+        ),
+          ],
         ),
         GoRoute(
           path: 'orders',
